bc457bed1b44782a6b27e09fb4c3ea14a31023c8
Eugene Wen <newenegue@gmail.com>
2014-02-19 15:46:02 -0800

    Added rails new app with postgresql db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beam::Application.config.secret_key_base = '60179fea3babbb453e5ea0b524c69c1b120d8c2904787afd1aa462ec05b1187107dad2c7f4b00b5b31046d9f0450c852320c62b750aa20b6dcc6b2f3a8f50547'
