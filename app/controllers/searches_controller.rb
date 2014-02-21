0f0e9b9861c713ee666be3707abf831647feee41
Eugene Wen <newenegue@gmail.com>
2014-02-20 18:23:00 -0800

    when user logins with instagram they can search their friends photos

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,12 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @instagram_results = Instagram.tag_recent_media(params[:search])
+	if session[:access_token]
+		client = Instagram.client(:access_token => session[:access_token])
+		@user = client.user
+		@instagram_results = client.tag_recent_media(params[:search])
+	else
+		@instagram_results = Instagram.tag_recent_media(params[:search])
+	end
   end
 end
