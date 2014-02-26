faa4e6d191d631c634d960e38b845ae500235c29
Eugene Wen <newenegue@gmail.com>
2014-02-25 18:53:21 -0800

    worked fullscreen slideshow toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,45 +13,119 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-// require turbolinks
+//= require turbolinks
 //= require sly
+//= require screenfull
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
 
 (function () {
+
 	var $frame = $('.frame');
 	var $wrap  = $frame.parent();
 
-	// Call Sly on frame
-	$frame.sly({
+	// -------------------------------------------------------------
+	// Default slideshow options
+	// -------------------------------------------------------------
+	var options = {
 		horizontal: 1,
 		itemNav: 'forceCentered',
 		smart: 1,
 		activateMiddle: 1,
-		activateOn: 'click',
+		activateOn: 'click', // sdlkahsg
 		mouseDragging: 1,
 		touchDragging: 1,
 		releaseSwing: 1,
-
 		startAt: 0,
 		scrollBar: $wrap.find('.scrollbar'),
 		scrollBy: 0,
-
-		moveBy: 500,
+		moveBy: 500, //alkjsdhg
 		speed: 150,
 		elasticBounds: 1,
 		easing: 'swing',
 		dragHandle: 1,
 		dynamicHandle: 1,
 		clickBar: 1,
-		keyboardNavBy: 'items',
 
-		// cycleBy: 'items',
-		// cycleInterval: 5000,
-
+		keyboardNavBy: 'items'
 		// Buttons
-		prev: $wrap.find('.prev'),
-		next: $wrap.find('.next')
+		// prev: $wrap.find('.prev'),
+		// next: $wrap.find('.next')
+	};
+
+	// -------------------------------------------------------------
+	//   Fullscreen one item per page slideshow
+	// -------------------------------------------------------------
+	// var options_fs = {
+	// 	horizontal: 1,
+	// 	itemNav: 'forceCentered',
+	// 	smart: 1,
+	// 	activateMiddle: 1,
+	// 	mouseDragging: 1,
+	// 	touchDragging: 1,
+	// 	releaseSwing: 1,
+	// 	startAt: 0,
+	// 	scrollBar: $wrap.find('.scrollbar'),
+	// 	scrollBy: 0,
+	// 	speed: 150,
+	// 	elasticBounds: 1,
+	// 	easing: 'swing',
+	// 	dragHandle: 1,
+	// 	dynamicHandle: 1,
+	// 	clickBar: 1,
+	// 	// cycleBy: 'items',
+	// 	// cycleInterval: 1000,
+	// 	keyboardNavBy: 'items'
+	// };
+
+	// -------------------------------------------------------------
+	// Create and initialize Sly slideshow
+	// -------------------------------------------------------------
+	var sly = new Sly($frame, options).init();
+
+	// -------------------------------------------------------------
+	// -------------------------------------------------------------
+	sly.on("active", function() {
+		if(screenfull.isFullscreen) {
+			$('.active-fullscreen').removeClass('active-fullscreen');
+			// $('li').addClass('fullscreen');
+			$('.active').addClass('active-fullscreen');
+			// $('.active').removeClass('fullscreen');
+		}
 	});
+
+	$('#fullscreen').click(function () {
+		if (screenfull.enabled) {
+			var frame = $('.frame')[0];
+			screenfull.request(frame);
+		}
+		sly.reload();
+	});
+
+	if (screenfull.enabled) {
+		document.addEventListener(screenfull.raw.fullscreenchange, function () {
+			// console.log('Am I fullscreen? ' + (screenfull.isFullscreen ? 'Yes' : 'No'));
+			if(screenfull.isFullscreen){
+				// $('li').addClass('fullscreen');
+				$('.active').addClass('active-fullscreen');
+				$('.active').removeClass('fullscreen');
+				$('.frame').addClass('frame_fs');
+				$('.frame_fs').removeClass('frame');
+			}
+			else {
+				$('.active').removeClass('active-fullscreen');
+				// $('li').removeClass('fullscreen');
+				$('.frame_fs').addClass('frame');
+				$('.frame').removeClass('frame_fs');
+			}
+			sly.reload();
+		});
+	}
 }());
+
+
+
+
+
+
