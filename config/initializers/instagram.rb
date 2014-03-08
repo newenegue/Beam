981daad15ed34073066fcb758d08972740a64d48
Eugene Wen <newenegue@gmail.com>
2014-03-07 18:00:02 -0800

    using figaro for heroku beam app

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,9 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
-  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID_BEAM']
+  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET_BEAM']
 end
 
-CALLBACK_URL = "http://localhost:3000/session/callback"
+# CALLBACK_URL = "http://localhost:3000/session/callback"
+CALLBACK_URL = "http://beam-team.herokuapp.com/"
